4ac34845b41b69b021e98a4edbe67f811dfb8dbc
AleksandraMN <al_mn@mail.ru>
2023-12-30 17:58:37 +1000

    task1

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+{
+   const productName1 = 'яблоки';
+   const quantity = 10;
+   const category = 'продукты';
+   const priceUnit = 290;
+   console.log(`Наименование товара: ${productName1}.`);
+
+   const amount1 = quantity * priceUnit;
+   console.log(`Всего на общую сумму = ${amount1} рублей.`);
+}
+{
+   const productName2 = 'мандарины';
+   const quantity = 24;
+   const category = 'продукты';
+   const priceUnit = 350;
+   console.log(`Наименование товара: ${productName2}.`);
+
+   const amount2 = quantity * priceUnit;
+   console.log(`Всего на общую сумму = ${amount2} рублей.`);
+}
+{
+   const productName3 = (prompt("Введите наименование товара буквами русского алфавита", "")).toLowerCase();
+   const quantity = Number(prompt("Введите количество товара в кг цифрами", ""));
+   console.log('Количество товара', quantity, typeof quantity);
+   const category = (prompt("Введите категорию товара буквами русского алфавита", "")).toLowerCase();
+   const priceUnit = Number(prompt("Введите цену единицы товара в рублях цифрами", ""));
+   console.log('Цена единицы товара', priceUnit, typeof priceUnit);
+
+   const amount3 = quantity * priceUnit;
+   console.log(`На складе ${quantity} единиц(ы) товара "${productName3}" на сумму = ${amount3} рублей.`);
+}
